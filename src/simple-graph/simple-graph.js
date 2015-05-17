a9854c9a12417357f34e99d4301d6ed84cba7c06
Audrey Carlsen <audreycarlsen@gmail.com>
2015-05-17 14:57:37 -0700

    adds in ability to read from google sheets

--- src/simple-graph/simple-graph.js
+++ src/simple-graph/simple-graph.js
@@ -4,6 +4,7 @@ require("document-register-element");
 //element setup
 var template = require("./_template.html");
 require("./simple-graph.less");
+var Tabletop = require("./tabletop").Tabletop;
 
 //libraries
 var csv = require("./csv");
@@ -14,12 +15,38 @@ var parser = new csv.Parser();
 var proto = Object.create(HTMLElement.prototype);
 
 proto.createdCallback = function() {
-  var html = this.innerHTML;
-  this.innerHTML = template();
-  html = html.split("\n").map(s => s.trim()).filter(s => s).join("\n");
-  var data = parser.parse(html);
-  
-  var graph = new Graph(this, data);
+  if (this.hasAttribute("sheet")) {
+    var parsed = [];
+    var sheetId = this.getAttribute("sheet");
+    var self = this;
+    Tabletop.init({ 
+      key: sheetId, 
+      callback: function(data, tabletop) {
+        var header = [];
+        for (var label in data[0]) {
+          header.push(label);
+        }
+        parsed.push(header);
+        data.forEach(function(row) {
+          var parsedRow = [];
+          for (var label in row) {
+            parsedRow.push(row[label]);
+          }
+          parsed.push(parsedRow);
+        });       
+        self.innerHTML = template();
+        var graph = new Graph(self, parsed);
+      },
+      simpleSheet: true,
+      parseNumbers: true
+    });
+  } else {
+    var html = this.innerHTML;
+    html = html.split("\n").map(s => s.trim()).filter(s => s).join("\n");
+    var data = parser.parse(html);
+    this.innerHTML = template();
+    var graph = new Graph(this, data);
+  }
 };
 proto.attachedCallback = function() {};
 proto.detachedCallback = function() {};
